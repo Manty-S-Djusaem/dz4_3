f16022350cc06081d984b971beddb53de64837da
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-03-04 13:08:45 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", "peoples.json")
+    request.setRequestHeader('Content-type', 'application/json')
+    request.send()
+    request.addEventListener('load', () => {
+        const data = JSON.parse(request.response)
+        const wrapper = document.querySelector('.wrapper')
+
+        data.forEach((item) => {
+            const div = document.createElement('div')
+            div.setAttribute('class', 'user')
+            div.innerHTML = `
+                <img src="image/avatar.jpg" alt=""
+                <h2 class="text">Name: ${item.name}</h2>
+                <span>Age:${item.age}</span>
+            `
+            wrapper.append(div)
+        })
+    })
+})
+
